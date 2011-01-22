d8a40e1a61e9bd786bf348f6cf94cb5be075ca45
blackops7799@gmail.com <blackops7799@gmail.com@9e717b2d-eb2a-0410-b2d1-0368184b35f6>
2011-01-22 23:03:54 +0000

    Flag base shouldn't glow Flag glows proper color

--- trunk/Paintball/entities/entities/flag/cl_init.lua
+++ trunk/Paintball/entities/entities/flag/cl_init.lua
@@ -8,10 +8,11 @@ local glowMat = Material( "sprites/light_glow02_add" )
 function ENT:Draw()
 
 	local r,g,b,a = self:GetColor()
+	local clr = Color( r,g,b,a )
 	render.SetMaterial( glowMat )
-	render.DrawSprite( self:GetPos(), 56, 56, Color( r,g,b,a ) )
+	render.DrawSprite( self:GetPos(), 56, 56, clr )
 	
 	self:DrawModel()
-	self:RenderGlow()
+	self:RenderGlow( clr )
 	
 end
