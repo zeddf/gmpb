cc2ddd22f39687263baaa9cd63c8807c361cebcc
blackops7799@gmail.com <blackops7799@gmail.com@9e717b2d-eb2a-0410-b2d1-0368184b35f6>
2011-01-22 22:22:28 +0000

    Added glowing sprite back to flags

--- Paintball/entities/entities/flag/cl_init.lua
+++ Paintball/entities/entities/flag/cl_init.lua
@@ -1,7 +1,16 @@
+
 function ENT:Initialize()
 end
 
-function ENT:Draw()		
+local glowMat = Material( "sprites/light_glow02_add" )
+
+function ENT:Draw()
+
+	local r,g,b,a = self:GetColor()
+	render.SetMaterial( glowMat )
+	render.DrawSprite( self:GetPos(), 56, 56, Color( r,g,b,a ) )
+	
 	self:DrawModel()
 	self:RenderGlow()
+	
 end
