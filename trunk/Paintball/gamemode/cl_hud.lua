fbab0293c8fd5c41e8ba4f7001b1f421d677bce5
blackops7799@gmail.com <blackops7799@gmail.com@9e717b2d-eb2a-0410-b2d1-0368184b35f6>
2011-01-23 23:04:59 +0000

    Fixed derp error in mask drawing Raised max physics velocity

--- trunk/Paintball/gamemode/cl_hud.lua
+++ trunk/Paintball/gamemode/cl_hud.lua
@@ -1,7 +1,7 @@
 local MaskMat = surface.GetTextureID( "paintball/mask" )
 
 hook.Add( "HUDPaint", "GMPB_HUD", function()
-	if LocalPlayer():Alive() and ( ply:Team() == TEAM_RED or ply:Team() == TEAM_BLUE ) then
+	if LocalPlayer():Alive() and ( LocalPlayer():Team() == TEAM_RED or LocalPlayer():Team() == TEAM_BLUE ) then
 		surface.SetTexture( MaskMat )
 		surface.SetDrawColor( color_white )
 		surface.DrawTexturedRect( 0, 0, ScrW(), ScrH())
