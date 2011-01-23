1ab2d20e6ff8ad9e85cd610eb8d16c2eb554087d
blackops7799@gmail.com <blackops7799@gmail.com@9e717b2d-eb2a-0410-b2d1-0368184b35f6>
2011-01-23 20:06:50 +0000

    Fixed flag not dropping correctly Added sounds to flag events Added money system base Allowed suicide

--- trunk/Paintball/entities/entities/flag/init.lua
+++ trunk/Paintball/entities/entities/flag/init.lua
@@ -4,6 +4,7 @@ include( "shared.lua" )
 
 AccessorFunc( ENT, "TeamID", "Team" )
 AccessorFunc( ENT, "FlagBase", "FlagBase" )
+AccessorFunc( ENT, "m_bIsAtBase", "IsAtBase" )
 
 function ENT:Initialize()
 	self:SetModel( "models/roller.mdl" )
@@ -11,15 +12,19 @@ function ENT:Initialize()
 	self:PhysicsInit( SOLID_VPHYSICS )
 	self:SetMoveType( MOVETYPE_NONE )
 	self:SetSolid( SOLID_VPHYSICS )
-	self:SetCollisionGroup( COLLISION_GROUP_WORLD )
+	self:SetCollisionGroup( COLLISION_GROUP_WEAPON )
 	self:SetTrigger( true )
 	
-	util.PrecacheModel( "models/roller_spikes.mdl" )
+	local phys = self:GetPhysicsObject()
+	if IsValid( phys ) then
+		phys:Wake()
+	end
 end
 
 function ENT:SetUp( teamid, base )
 	self:SetTeam( teamid )
 	self:SetFlagBase( base )
+	self:SetIsAtBase( true )
 	if teamid == TEAM_BLUE then
 		self:SetColor( 112, 160, 255, 255 )
 	else
@@ -28,30 +33,33 @@ function ENT:SetUp( teamid, base )
 end
 
 function ENT:Return( ply, shouldcall )
+	print( "Return", ply )
 	self:SetParent()
 	self:SetOwner()
 	self:SetMoveType( MOVETYPE_NONE )
-	self:SetSolid( SOLID_VPHYSICS )
-	self:SetCollisionGroup( COLLISION_GROUP_WORLD )
+	self:SetSolid( SOLID_NONE )
 	self:SetTrigger( true )
-	self:SetModel( "models/roller.mdl" )
+	self:SetIsAtBase( true )
 	self:SetPos( self:GetFlagBase():GetPos() + Vector( 0, 0, 55 ) )
+	self:SetAngles( angle_zero )
 	if shouldcall then
 		hook.Call( "OnPlayerFlagReturned", GAMEMODE, ply, self )
 	end
 end
 
 function ENT:PlayerCapture( ply )
+	print( "PlayerCapture", ply )
 	self:Return( ply, false )
 	hook.Call( "OnPlayerFlagCapture", GAMEMODE, ply, self )
 end
 
 function ENT:PlayerTake( ply )
+	print( "PlayerTake", ply )
 	ply:SetFlag( self )
 	self:SetOwner( ply )
 	self:SetParent( ply )
-	self:SetModel( "models/roller_spikes.mdl" )
 	self:SetTrigger( false )
+	self:SetIsAtBase( false )
 	local eyes = ply:LookupAttachment( "anim_attachment_head" )
 	if eyes != 0 then
 		self:SetPos( ply:GetAttachment( eyes ).Pos + vector_up * 32 )
@@ -62,24 +70,30 @@ function ENT:PlayerTake( ply )
 end
 
 function ENT:PlayerDropped( ply )
+	print( "PlayerDropped", ply )
 	ply:SetFlag()
-	self:SetMoveType( MOVETYPE_VPHYSICS )
-	self:SetSolid( SOLID_BBOX )
-	self:SetCollisionGroup( COLLISION_GROUP_WEAPON )
-	self:SetCollisionBounds( Vector( -24, -24, -40 ), Vector( 24, 24, 24 ) )
+	local Pos = self:GetPos()
+	local Ang = self:GetAngles()
 	self:SetTrigger( true )
 	self:SetParent()
 	self:SetOwner()
-	self:PhysWake()
+	self:PhysicsInit( SOLID_VPHYSICS )
+	self:SetMoveType( MOVETYPE_VPHYSICS )
+	self:SetSolid( SOLID_VPHYSICS )
+	self:SetPos( Pos )
+	self:SetAngles( Ang )
+	local phys = self:GetPhysicsObject()
+	if IsValid( phys ) then
+		phys:Wake()
+	end
 	hook.Call( "OnPlayerFlagDropped", GAMEMODE, ply, self )
 end
 
 function ENT:StartTouch( ent )
-	print( self, ent )
 	if ent:IsPlayer() and !IsValid( ent:GetOwner() ) then
 		if ent:Team() != self:GetTeam() then
 			self:PlayerTake( ent )
-		elseif ent:Team() == self:GetTeam() then
+		elseif ent:Team() == self:GetTeam() and !self:GetIsAtBase() then
 			self:Return( ent, true )
 		end
 	end
