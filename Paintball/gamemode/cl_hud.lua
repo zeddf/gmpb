59e9439f4114f81d451456c0251471219859d806
gmpb.thepanda@googlemail.com <gmpb.thepanda@googlemail.com@9e717b2d-eb2a-0410-b2d1-0368184b35f6>
2011-01-24 19:39:19 +0000

    Money put into place. (Loads player's file from the server from the old gamemode)
    Started working on the Mini-Hud.

--- Paintball/gamemode/cl_hud.lua
+++ Paintball/gamemode/cl_hud.lua
@@ -6,6 +6,16 @@ hook.Add( "HUDPaint", "GMPB_HUD", function()
 		surface.SetTexture( MaskMat )
 		surface.SetDrawColor( color_white )
 		surface.DrawTexturedRect( 0, 0, ScrW(), ScrH())
+		
+		surface.CreateFont ("Trebuchet", 24, 400, true, false, "T24")
+		draw.RoundedBox(0, ScrW() / 2 - 32, 32, 64, 32, Color(255, 165, 0, 191))
+		draw.SimpleText("$" ..LocalPlayer():GetNetworkedFloat("GMPBMoney"), "T24", ScrW() / 2 - 1, 47, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+		
+		draw.RoundedBoxEx(6, ScrW() / 2 - 64, 32, 32, 32, team.GetColor(1), false, false, true, false)
+		draw.SimpleText(tostring(GetGlobalInt(team.GetName (1) .. "Score")), "T24", ScrW() / 2 - 48, 47, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+		
+		draw.RoundedBoxEx(6, ScrW() / 2 + 32, 32, 32, 32, team.GetColor(2), false, false, false, true)
+		draw.SimpleText(tostring(GetGlobalInt(team.GetName (2) .. "Score")), "T24", ScrW() / 2 + 48, 47, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 	end
 	
 end )
